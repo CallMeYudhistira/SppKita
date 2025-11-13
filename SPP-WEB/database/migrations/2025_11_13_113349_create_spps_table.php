488f22cf31f583a81d77bf8d6d727897d99e455e
CallMeYudhistira <yudhistira30122008@gmail.com>
2025-11-13 18:58:47 +0700

    feat: make migration and model for table

--- SPP-WEB/database/migrations/2025_11_13_113349_create_spps_table.php
+++ SPP-WEB/database/migrations/2025_11_13_113349_create_spps_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('spp', function (Blueprint $table) {
+            $table->integer('id_spp')->autoIncrement();
+            $table->string('tahun', 11);
+            $table->integer('nominal');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('spp');
     }
 };
